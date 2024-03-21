d69f8c2e59987d4d4160f3e439cc5ea7408c0d47
Akib Hossain Shuvo <142579723+akibsikder@users.noreply.github.com>
2024-03-21 23:08:30 +0600

    otp

--- backEnd/controller/regController.js
+++ backEnd/controller/regController.js
@@ -3,6 +3,8 @@ const emailValid = require("../helpers/emailValid")
 const passwordLength = require("../helpers/passwordLengh")
 const User = require("../model/userModel")
 const bcrypt = require('bcrypt');
+const otpGenerator = require('otp-generator')
+const nodemailer = require("nodemailer");
 
 let regController = async (req,res)=>{
    const {name, email, password} = req.body
@@ -29,6 +31,31 @@ let regController = async (req,res)=>{
       if(exitingEmail.length > 0){
       res.send({emailError:`${email} already exist`})
       }else{
+         const otp = otpGenerator.generate(6, { 
+            lowerCaseAlphabets: false,
+                upperCaseAlphabets: false,
+                specialChars: false 
+               });
+   
+            console.log(otp)
+
+         const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            auth: {
+              user: "maddison53@ethereal.email",
+              pass: "jn7jnAPss4f63QBp6D",
+            },
+          });
+
+          const info = await transporter.sendMail({
+            from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+            to: "bar@example.com, baz@example.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            html: "<b>Hello world?</b>", // html body
+          });
+
+
          bcrypt.hash(password, 10, function(err, hash) {
           let user = new User({
             name:name,
@@ -36,7 +63,7 @@ let regController = async (req,res)=>{
             password:hash
          })
          user.save()
-   
+         
          res.send({
             name:user.name,
             email:user.email,
@@ -47,8 +74,6 @@ let regController = async (req,res)=>{
         
 
       }
-
-      
    }
    
 }
