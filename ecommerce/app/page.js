765645e9c78355effbdf118cbe6c8b4573fb9eee
akibamir <amirakibbin@gmail.com>
2024-08-24 15:19:46 +0600

    nextJS

---
+++ ecommerce/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+   <Link href='/about'>swift about</Link> <br/>
+   <Link href='/cartPage'>cart cart</Link>
+   </>
+  );
+}
